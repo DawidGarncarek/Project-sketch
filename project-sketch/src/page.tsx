9023e7054e760432e59b592e36d951f9d5ec41ab
Dawiddek <150728883+Dawiddek@users.noreply.github.com>
2024-10-28 23:43:32 +0100

    Creating page.txs and tailawind.config.js

---
+++ project-sketch/src/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+  return (
+    <div className="text-center p-4 bg-gray-100 min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">Witaj na mojej stronie!</h1>
+      <p className="text-gray-700">To jest przykładowa strona stworzona z React i Tailwind CSS.</p>
+    </div>
+  );
+};
+
+export default Page;
--- project-sketch/tailwind.config.js
+++ project-sketch/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Monitoruje pliki w folderze src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
