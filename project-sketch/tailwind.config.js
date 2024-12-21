7a814ca96d6e9826e97fd25790369812babaa764
Dawiddek <150728883+Dawiddek@users.noreply.github.com>
2024-12-21 14:05:05 +0100

    Poprawki tailawind.config.js

--- project-sketch/tailwind.config.js
+++ project-sketch/tailwind.config.js
@@ -3,10 +3,10 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
+  mode: 'jit', 
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     container: {
